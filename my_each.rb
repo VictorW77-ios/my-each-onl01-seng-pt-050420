8c3838ab7e48452c951b9c6e9d2fc3c3957f5ef3
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 15:10:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,9 @@
 def my_each(array) # put argument(s) here
 array = []
     i = 0 
-
+  my_each(array) do |word|
+    "#{word}"
+ 
    while i < (array.length).times
     yield(array[i])
     i = i + 1
